9ae7690f990b18b79da2983237b7a5b02885a271
aemre <aemre38@hotmail.com>
2021-03-08 22:55:24 +0200

    enson

--- map.js
+++ map.js
@@ -4,21 +4,11 @@ const { dataStudents,
         } = require('./data');
 
 let result = dataStudents().map(student => {
-    let longGender = null;
-    if (student.gender == 'E') longGender='Erkek';
-    else if (student.gender == 'K') longGender='Kadın';
     return {
             Id : student.id,
             Department : dataDepartments().find(department => (department.id == student.departmentId)).shortName,
             'Full Name' : student.name + ' ' + student.surName,
-            //TODO : arrow function ile Gender E ise Erkek, K ise Kadın yazdirilacak
-            Gender : (student) => { 
-                        if (student.gender == 'E') 
-                            return 'Erkek'
-                        else 
-                            return 'Kadın' },
-            //Gender :  student.gender == 'K'?'Kadın':'Erkek',
-            //Gender : longGender,
+            Gender : student.gender == 'K'?'Kadın':'Erkek',
             Age : (new Date()).getFullYear() - student.birthYear,
             StartYear : student.startYear 
         }
